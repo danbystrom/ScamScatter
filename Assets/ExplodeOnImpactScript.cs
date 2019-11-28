e754d5154b57bc44596d528817cb9ff7fb93d5dc
Dan Byström <dan@danbystrom.se>
2019-11-28 15:26:43 +0100

    added another sample scene

---
+++ Assets/ExplodeOnImpactScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ExplodeOnImpactScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        new ScamScatter.Scatter().Run(
+            this,
+            _ => ScamScatter.Explode.Run(collision.contacts[0].point, GetComponent<Collider>().bounds.extents.magnitude, 1), gameObject);
+    }
+
+}
